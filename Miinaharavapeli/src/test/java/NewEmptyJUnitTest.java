66f959b4a7cd04920f16391e0ddaa7ffbca09fca
Ingervo Eliel E O <ebingerv@dx5-cs-bk107-34.ad.helsinki.fi>
2019-04-05 15:28:32 +0300

    dokumentaatiota korjattu

--- Miinaharavapeli/src/test/java/NewEmptyJUnitTest.java
+++ Miinaharavapeli/src/test/java/NewEmptyJUnitTest.java
@@ -4,11 +4,12 @@
  * and open the template in the editor.
  */
 
+import javafx.scene.control.Button;
 import minesweeper.MineField;
+import minesweeper.Pane;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,46 +19,54 @@ import org.junit.Test;
  */
 public class NewEmptyJUnitTest {
     
-    MineField minefield;
+//    MineField minefield;
     
     public NewEmptyJUnitTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
-    }
-    
+    }  
     @AfterClass
     public static void tearDownClass() {
     }
     
-    @Before
-    public void setUp() {
-        minefield = new MineField(10, 5);
-    }
+//    @Before
+//    public void setUp() {
+//        minefield = new MineField(10, 5);
+//        System.out.println("Moi maailma?????!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + minefield);
+//    }
     
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+//     TODO add test methods here.
+//     The methods must be annotated with annotation @Test. For example:
+    
+     @Test
+     public void hello() {}
+     
+     @Test
+     public void setAndGetValue() {
+         Pane pane = new Pane(10, 1, new Button("miina"));
+//         pane.setMine(true);
+         assertEquals(false, pane.getMine());
+     }
     
     @Test
     public void endTheGame() {
+        MineField minefield = new MineField(10, 5);
         minefield.endGame();
         assertEquals(false, minefield.getAlive());
     }
-    
-    @Test
-    public void placeMines() {
-        minefield.placeMines();
-        assertEquals(7, minefield.getMines().size());
-        
-    }
+//    
+//    @Test
+//    public void placeMines() {
+//        minefield.placeMines();
+//        assertEquals(7, minefield.getMines().size());
+//        
+//    }
     
 //    kortti.lataaRahaa(100);
 //        assertEquals("saldo: 1.10", kortti.toString());
